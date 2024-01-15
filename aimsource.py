a4d04812f0f4598b7458abd9ec0343c36e1582fe
whoswhip <124531971+whoswhip@users.noreply.github.com>
2024-01-15 14:53:16 -0500

    Add files via upload

--- aimsource.py
+++ aimsource.py
@@ -22,16 +22,6 @@ kernel = np.ones((3, 3), np.uint8) # 3x3 array of 1s for structuring purposes
 # its important that you change (if youre using pyinstaller) os.path.dirname(__file__) to os.path.dirname(os.path.dirname(__file__))
 config_file_path = os.path.join(os.path.dirname(__file__), "config.ini") # Searching for the file called config.ini to read settings
 
-try: # checks for updates using the version number we defined earlier, pasted from andrewdarkyy cuz im lazy and his colorbot is just a modded version of mine so like who cares
-    if not "7" in urlopen("https://raw.githubusercontent.com/Seconb/Arsenal-Colorbot/main/version.txt").read().decode("utf-8"):
-        print("Outdated version, redownload: https://github.com/Seconb/Arsenal-Colorbot/releases")
-        while True:
-            time.sleep(0.1)
-except Exception as e:
-    print("Error checking update: ", e)
-    print("Continuing anyway!")
-    time.sleep(5)
-    pass
 
 try:
     config = configparser.ConfigParser() #this is separating all the config options you set.
